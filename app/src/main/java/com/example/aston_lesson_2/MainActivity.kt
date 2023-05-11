71c47035172415a5047715d1860768c675f32a05
danilbelikov <danilkikaoskol@mail.ru>
2023-05-11 12:29:27 +0300

    lesson 1.4

--- app/src/main/java/com/example/aston_lesson_2/MainActivity.kt
+++ app/src/main/java/com/example/aston_lesson_2/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.aston_lesson_2.lesson_11.Lesson11Activity
+import com.example.aston_lesson_2.lesson_11.TAG
 import com.example.aston_lesson_2.lesson_12_A.Lesson12Activity
 import com.example.aston_lesson_2.lesson_12_B.Lesson12BActivity
 import com.example.aston_lesson_2.lesson_13.Lesson13Activity
@@ -18,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         val button12 = findViewById<Button>(R.id.b1_2)
         val button12b = findViewById<Button>(R.id.b12B)
         val button13 = findViewById<Button>(R.id.b13)
+        val button14 = findViewById<Button>(R.id.b14)
+
 
         button11.setOnClickListener {
             val intent11 = Intent(this, Lesson11Activity::class.java)
@@ -36,6 +40,9 @@ class MainActivity : AppCompatActivity() {
         button13.setOnClickListener {
             val intent13 = Intent(this, Lesson13Activity::class.java)
             startActivity(intent13)
+        }
+        button14.setOnClickListener {
+            Toast.makeText(this, "Иконка приложения изменена", Toast.LENGTH_LONG).show()
         }
 
     }
