c16c127b9be4d95216522b07856565c93f57c83c
danilbelikov <danilkikaoskol@mail.ru>
2023-05-11 11:58:42 +0300

    lesson 1.3

--- app/src/main/java/com/example/aston_lesson_2/MainActivity.kt
+++ app/src/main/java/com/example/aston_lesson_2/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import com.example.aston_lesson_2.lesson_11.Lesson11Activity
 import com.example.aston_lesson_2.lesson_12_A.Lesson12Activity
 import com.example.aston_lesson_2.lesson_12_B.Lesson12BActivity
+import com.example.aston_lesson_2.lesson_13.Lesson13Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val button11 = findViewById<Button>(R.id.b1_1)
         val button12 = findViewById<Button>(R.id.b1_2)
         val button12b = findViewById<Button>(R.id.b12B)
+        val button13 = findViewById<Button>(R.id.b13)
 
         button11.setOnClickListener {
             val intent11 = Intent(this, Lesson11Activity::class.java)
@@ -29,6 +31,11 @@ class MainActivity : AppCompatActivity() {
         button12b.setOnClickListener {
             val intent12b = Intent(this, Lesson12BActivity::class.java)
             startActivity(intent12b)
+        }
+
+        button13.setOnClickListener {
+            val intent13 = Intent(this, Lesson13Activity::class.java)
+            startActivity(intent13)
         }
 
     }
